00eb7f2bf92f445b7284056dc8d6fed8a33d4ee1
Jérémy <jeremy@MacBook-Jeremy.local>
2020-01-08 10:35:27 +0100

    add bootstrap, img, nav , footer and page connection

---
+++ footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="font/styleheet.css">
+  <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="css/style.css">
+    <title>footer</title>
+
+
+  </head>
+
+    <footer>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <p>DC Comics</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <div class="col-md-1">
+
+    </div>
+    <div class="col-md-8">
+<form class="" action="x" method="post">
+  <input type="text" name="prenom" id="prenom" placeholder="Prénom" autocomplete="off" >
+
+  <input type="text" name="nom" id="nom" placeholder="Nom" autocomplete="off">
+
+  <input type="text" name="sujet" id="sujet" placeholder="Votre Sujet" autocomplete="off">
+
+      <textarea id="msg" name="user_message" placeholder="Votre message" ></textarea>
+
+      <div class="col-md-12">
+
+      </div>
+
+</form>
+    </div>
+
+  </div>
+</div>
+
+    </footer>
+
+  <script src="https://kit.fontawesome.com/bbafdfc119.js" crossorigin="anonymous"></script>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>nav</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+
+    <header>
+
+            <p>DC COMICS <span>ESPACE FORUM</span> </p>
+
+        <a class="connexion" href="#">Connexion</a>
+
+
+      <nav>
+        <ul>
+          <li> <a href="index.php">Accueil</a> </li>
+          <li> <a href="#">Forum</a> </li>
+          <!-- <li> <a href="inscription.php">Inscription</a> </li> -->
+          <li> <a href="#">Connexion</a> </li>
+        </ul>
+      </nav>
+
+    </header>
+
+  </body>
+</html>
