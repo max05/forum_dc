b231aa527c8850be578ce15cc46a333d3f7cf908
Jérémy <jeremy@MacBook-Jeremy.local>
2020-01-08 12:00:29 +0100

    add footer.php, rename index.php by accueil.php, modified accueil.php

--- footer.php
+++ footer.php
@@ -16,33 +16,57 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-4">
       <p>DC Comics</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<ul>
+  <li> <a href="accueil.php">Accueil</a> </li>
+  <li> <a href="forum.php">Forum</a> </li>
+  <li> <a href="page-connexion.php">Connexion</a> </li>
+</ul>
     </div>
 
-    <div class="col-md-1">
 
-    </div>
     <div class="col-md-8">
-<form class="" action="x" method="post">
-  <input type="text" name="prenom" id="prenom" placeholder="Prénom" autocomplete="off" >
 
-  <input type="text" name="nom" id="nom" placeholder="Nom" autocomplete="off">
+      <h4>N'hésites pas à nous contacter si tu souhaites participer à l'évolution du site!</h4>
+<form class="form-footer" action="x" method="post">
+  <div class="deuxblocs">
+    <input type="text" name="prenom" id="prenom" placeholder="Prénom *" autocomplete="off" >
 
-  <input type="text" name="sujet" id="sujet" placeholder="Votre Sujet" autocomplete="off">
+    <input type="text" name="nom" id="nom" placeholder="Nom *" autocomplete="off">
+  </div>
+<div class="deuxblocs">
+  <input type="email" name="email" id="email" placeholder="Email *" autocomplete="off">
+  <input type="text" name="sujet" id="sujet" placeholder="Votre Sujet *" autocomplete="off">
+</div>
+<div class="deuxblocs">
+  <textarea id="msg" name="user_message" placeholder="Votre message" ></textarea>
+  <input class="reset" TYPE="reset" NAME="reset" VALUE="Réinitialiser">
+  <input class="envoyer" TYPE="submit" NAME="envoyer" VALUE="Envoyer">
 
-      <textarea id="msg" name="user_message" placeholder="Votre message" ></textarea>
-
-      <div class="col-md-12">
-
-      </div>
+</div>
 
 </form>
     </div>
+    <div class="col-md-12 reseaux-sociaux">
+      <p class="rejoins-nous">Rejoins-nous sur nos autres réseaux :</p> <br>
 
+        <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://youtube.com" target="_blank"><i class="fab fa-youtube"></i></a>
+
+    </div>
+    <div class="col-md-4 liens-annexes">
+      <a href="#">Mentions légales</a>
+      <a href="#">Crédits</a>
+      <a href="#">Plan du site</a>
+    </div>
   </div>
 </div>
+
+
 
     </footer>
 
--- page-connexion.php
+++ page-connexion.php
@@ -11,9 +11,10 @@
    <?php include 'nav.php'; ?>
 
 
-    <section id="formulaire">
-    <h1>Connexion</h1>
+    <section id="formulaire-connexion">
+
       <form class="formulaire" method="post" action="connexion.php" autocomplete="off" autocomplete="false">
+            <h2>Connexion</h2>
             <label for="pseudo"> <!--Identifiant--> </label> <br> <input type="text" placeholder="Identifiant" size="24" name="email" id="email" autocomplete="off"/>
                <br/>
                  <label for="password"> <!--Password--> </label>  <br> <input type="password" placeholder="Mot de passe" name="password" id="password" autocomplete="off"/>
@@ -21,5 +22,7 @@
                  <input id="envoyer" type="submit" value="Se connecter"/>
 
       </section>
+
+           <?php include 'footer.php'; ?>
   </body>
 </html>
