16ddd2bbdfb92c7e22c3f0773c0ab7651ebfc622
Jérémy <jeremy@MacBook-Jeremy.local>
2020-01-09 14:16:21 +0100

    need img from BDD

--- page-forum.php
+++ page-forum.php
@@ -12,22 +12,27 @@ require 'bdd.php';
 </head>
 
 <body>
-  <section id="sectionp-forum">
-    <h1>page forum</h1>
-  </section>
+
+  <section id="section-forum">
+    <section id="sectiondiv-forum">
+
+
     <?php
        $query = $pda->prepare("SELECT `id`, `title` , `description` FROM `categories`");
        $query->execute();
        while($result = $query->fetch()){
          echo '<a href="page-thread.php?id='.$result['id'].'">';
-         echo '<section>';
-         echo '<h2> '.$result['title'].' </h2>';
-         echo '<h3> '.$result['description'] .'</h3>';
-         echo '</section>';
+         echo '<div>';
+         echo '<h3> '.$result['img'].' </h3>';
+         echo '<h3> '.$result['title'].' </h3>';
+         echo '<p> '.$result['description'] .'</p>';
+         echo '</div>';
          echo '</a>';
        }
        $query->closeCursor();
     ?>
+    </section>
+      </section>
 
 
 
