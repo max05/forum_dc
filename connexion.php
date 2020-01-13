00bcbd958309335b6e8cbe162169e6fc3e215e84
Maxime <max.poulmane@gmail.com>
2020-01-13 15:32:14 +0100

    add md5 for hash passwprd and delay pour inscription

--- connexion.php
+++ connexion.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 var_dump($_SESSION);
- require 'bdd.php'; 
- ?>
+require 'bdd.php';
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 
@@ -15,7 +15,7 @@ var_dump($_SESSION);
   <?php /*condition pour savoir si le mail et le mdp n'est pas vide, puis on exécute pour la requête et
 on fait une condition pour savoir si le mdp est dans la bdd, sinon on envoie un message d'erreur */
   if (isset($_POST["email"]) || !empty($_POST["password"])) {
-    $query = $pda->prepare("SELECT `id`, `pseudo`, `email`, `password` FROM `users` WHERE `email` = :mail");
+    $query = $pda->prepare("SELECT `id`, `pseudo`, `email`, `password` FROM `users` WHERE `email` = :mail LIMIT 1");
     $query->bindValue("mail", $_POST['mail']);
     try {
       $query->execute();
@@ -24,13 +24,13 @@ on fait une condition pour savoir si le mdp est dans la bdd, sinon on envoie un 
       var_dump($_POST['mail']);
       var_dump($_POST['password']);
       var_dump($query->fetchAll());
-      if ($user['password'] == $_POST['password']) {
+      if ($user['password'] == md5($_POST['password'])) {
         echo "Bienvenue : " . $user['pseudo'];
         $_SESSION['user'] = $user;
         var_dump($_SESSION);
         header("Location: accueil.php");
         exit;
-      } else if ($user['password'] != $_POST['password']) {
+      } else if ($user['password'] != md5($_POST['password'])) {
         echo "ERREUR MOT DE PASSE";
       }
     } catch (\Exception $ex) {
--- requete_inscription.php
+++ requete_inscription.php
@@ -1,38 +1,41 @@
 <?php
 session_start();
 require 'bdd.php'; ?>
-    <a href="accueil.php">Accueil</a>
-    <?php if(isset($_POST["prenom"]) && !empty($_POST["pseudo"]) && isset($_POST["email"]) && !empty($_POST["email-confirm"]) && isset($_POST["mdp"]) && !empty($_POST["mdp-confirm"])) {
+    <?php if (isset($_POST["prenom"]) && !empty($_POST["pseudo"]) && isset($_POST["email"]) && !empty($_POST["email-confirm"]) && isset($_POST["mdp"]) && !empty($_POST["mdp-confirm"])) {
       $query = $pda->prepare("SELECT `email` FROM `users` WHERE `email` = :email");
       $query->bindValue("email", $_POST['email']);
       try {
         $query->execute();
         $users = $query->fetch();
-        if($users['email'] != $_POST['email']){
+        if ($users['email'] != $_POST['email']) {
           $query = $pda->prepare("INSERT INTO `users`(`pseudo`, `password`, `gender`, `age`, `email`) VALUES(:pseudo, :mdp, :gender, :age, :email)");
           $query->bindValue("pseudo", $_POST['pseudo']);
-          $query->bindValue("mdp", $_POST['mdp']);
+          $query->bindValue("mdp", md5($_POST['mdp']));
           $query->bindValue("gender", $_POST['gender']);
-          if($_POST['age'] == ''){
+          if (is_string($_POST['age']) || ($_POST['age'] == '')) {
+
             $query->bindValue("age", NULL);
-          }
-          else{
+          } else {
             $query->bindValue("age", $_POST['age']);
           }
           $query->bindValue("email", $_POST['email']);
           try {
-              $query->execute();
-              echo "Inscription valide";
-              header("Location: accueil.php");
+            $query->execute();
+            echo '<h1>Inscription réussi</h1>';
+            echo '<h2>Vous allez être redirigé dans quelques instants ... </h2>';
+            echo '<script> 
+              window.setTimeout(function(){
+                window.location.href="accueil.php";
+              }, 2000);            
+              </script>';
           } catch (\Exception $e) {
             echo "Erreur dans l'inscription";
             var_dump($e);
           }
-        } else{
+        } else {
           echo "email déjà utilisé";
         }
       } catch (\Throwable $th) {
         //throw $th;
       }
-
     } ?>
