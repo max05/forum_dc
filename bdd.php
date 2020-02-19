e76b7836eb02db64cb48b197cf6b6751a1b1888d
Jérémy <jeremy@MacBook-Jeremy.local>
2020-02-19 11:21:15 +0100

    Update BDD for online

--- bdd.php
+++ bdd.php
@@ -1,7 +1,8 @@
 <?php
 
  try {
-  $pda = new PDO("mysql:host=localhost;dbname=amigraf_forum_dc;charset=utf8","root","root");
+  $pda = new PDO("mysql:host=batfolioai213.mysql.db;dbname=batfolioai213;charset=utf8","batfolioai213","Leclerc8");
+  // Après host, mettre l'adresse mysql, dbname mettre le nom de la BDD, ensuite nom utilisateur et mot de passe
 
 } catch (\Exception $e) {
   echo "<pre>";
